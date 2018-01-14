fb4d3de76573a2eabef00235765be5a1a2187822
FrederikTao <frederiktaodybdallarsen@gmail.com>
2018-01-14 15:49:39 -0800

    Express REST API with MongoDB and Mongoose Part 1

--- app.js
+++ app.js
@@ -8,6 +8,23 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 
+var dishRouter = require('./routes/dishRouter');
+// var promoRouter = require('./routes/promoRouter');
+// var leaderRouter = require('./routes/leaderRouter');
+
+const mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
+// Connection URL
+const url = 'mongodb://localhost:27017/conFusion';
+const connect = mongoose.connect(url, {
+    useMongoClient: true,
+    /* other options */
+  });
+
+connect.then((db) => {
+    console.log("Connected correctly to server");
+}, (err) => { console.log(err); });
+
 var app = express();
 
 // view engine setup
@@ -24,6 +41,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/dishes',dishRouter);
+// app.use('/promotions',promoRouter);
+// app.use('/leaders',leaderRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
